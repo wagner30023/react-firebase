505054220fc55a8ebd4920a09c7c974a98622773
Carlos Wagner <wagner30023@gmail.com>
2023-05-31 07:55:16 -0300

    usando firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,83 @@
+// Database
 import { db } from './firebaseConnection'
 
+// hooks
+import { useState } from 'react';
+
+
+// css 
+import './App.css';
+import { async } from '@firebase/util';
+import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
+
 function App() {
+
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd() {
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // })
+    // .then(() => {
+    //   console.log("Dados registrado no banco");
+    // })
+    // .then().catch((error) => {
+    //   console.log("Erro ocorrido " + error);
+    // })
+
+    //  gerar um ID único
+    await addDoc(collection(db, "posts"), {
+      titulo,
+      autor,
+    })
+    .then(() => {
+      console.log("Dados registrados com sucesso!");
+      setAutor('');
+      setTitulo('');
+    })
+    .catch((error) => {
+      console.log("Erro ocorrido " + error);
+    });
+
+  }
+
+  async function buscarPost(){
+    const postRef = doc(db,"posts","jnPAMQScrggUmuw5UXEp");
+
+    await getDoc(postRef)
+    .then((snapshot) => {
+      setAutor(snapshot.data().autor);
+      setTitulo(snapshot.data().titulo);
+    }).catch((error) => {
+      console.error("Erro ao buscar" + error);
+    });
+  }
+
   return (
-    <div className="App">
+    <div>
       <h1> React + Firebase :) </h1>
+      <div className='container'>
+        <label> Título: </label>
+        <textarea
+          type="text"
+          placeholder="Digite o título"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+
+        <label> Auttor: </label>
+        <input
+          type="text"
+          placeholder="Autor do post"
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
+        />
+
+        <button onClick={handleAdd}> Cadastrar </button>
+        <button onClick={buscarPost}> Buscar post </button>
+      </div>
     </div>
   );
 }
