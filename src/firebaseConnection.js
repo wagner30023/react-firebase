471145f8847af641009d55476d76ebe6fc4e5e44
Carlos Wagner <wagner30023@gmail.com>
2023-05-31 06:12:01 -0300

    configurando firebase na aplicação

---
+++ src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhLrEzx_jQELgQEnrvBpcmY7CYzoJCYmE",
+    authDomain: "curso-b8923.firebaseapp.com",
+    projectId: "curso-b8923",
+    storageBucket: "curso-b8923.appspot.com",
+    messagingSenderId: "411466295010",
+    appId: "1:411466295010:web:b45c92e9902fca8422a0bb",
+    measurementId: "G-N6LE7C3G1K"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
