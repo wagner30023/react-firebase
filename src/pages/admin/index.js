99062c83695fa9ced60fe6d4973db7ee4d2086f2
Carlos Wagner <wagner30023@gmail.com>
2023-06-29 18:33:24 -0300

    Registrando atividades no banco

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,12 +1,71 @@
+
+import { useState, useEffect } from 'react';
 import './admin.css';
 
-export default function Admin(){
+import { auth, db } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 
-    console.log('Page Admin');
+export default function Admin() {
+    const [tarefaInput, setTarefaInput] = useState('');
+    const [user, setUser] = useState({});
 
-    return(
-        <div>
-            <h2> Page Admin </h2>
+    useEffect(() => {
+        async function loadfTarefas() {
+            const userDetail = localStorage.getItem("@detailUser");
+            setUser(JSON.parse(userDetail));
+        }
+
+        loadfTarefas();
+    }, []);
+
+    async function handleRegister(e) {
+        e.preventDefault();
+        if (tarefaInput === '') {
+            alert("Digite a sua tarefa");
+            return;
+        }
+
+        await addDoc(collection(db, "tarefas"), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUID: user?.uid
+        })
+        .then(() => {
+            console.log("Tarefa registrada");
+            setTarefaInput('');
+        })
+        .catch((error) => {
+            console.log("Erro ao registrar" + error);
+        });
+    }
+
+    async function handleLogout() {
+        await signOut(auth);
+    }
+
+    return (
+        <div className="admin-container" >
+            <h1> Minhas tarefas </h1>
+            <form onSubmit={handleRegister} className="form">
+                <textarea
+                    placeholder="Digite a sua tarefa..."
+                    value={tarefaInput}
+                    onChange={(e) => setTarefaInput(e.target.value)}
+                />
+                <button type="submit" className="btn-register"> Registrar tarefa </button>
+            </form>
+
+            <article className="list">
+                <p> Estudar javascript e reactjs a noite </p>
+
+                <div>
+                    <button> Editar </button>
+                    <button className="btn-delete"> Concluir </button>
+                </div>
+            </article>
+
+            <button className="btn-logout" onClick={handleLogout}> Sair </button>
         </div>
     )
 }
